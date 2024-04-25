2388113992f09ae44fae6d6c1ead35d2b711d03e
EmperorBlizzard <emil111@live.se>
2024-04-25 11:18:15 +0200

    Add project files.

---
+++ FrontendApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontendApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FrontendApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FrontendApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
