c0a5403cdcf8ab0f6dd67c9bbce064b9bcbef8ee
Annelieno1 <gydano1@hotmail.com>
2024-05-07 15:49:14 +0200

    smaller changes

--- FrontendApp/ViewModels/SignUpViewModel.cs
+++ FrontendApp/ViewModels/SignUpViewModel.cs
@@ -16,11 +16,14 @@ public class SignUpViewModel
     public string LastName { get; set;} = null!;
 
     [Required]
+    [RegularExpression(@"^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?).[a-zA-Z]{2,}$", ErrorMessage = "An valid email address is required")]
     public string Email { get; set; } = null!;
+
 
     [Required]
     public string Password { get; set; } = null!;
 
     [Required]
+    [Compare (nameof(Password))]
     public string ConfirmPassword { get; set; } = null!;
 }
